dfeda38ce0c08f2766050721c9069313df833c9f
kmsheng <kmsh3ng@gmail.com>
2016-08-17 01:21:37 +0800

    add space after !

--- src/routes.js
+++ src/routes.js
@@ -24,7 +24,7 @@ export default (store) => {
       cb();
     }
 
-    if (!isAuthLoaded(store.getState())) {
+    if (! isAuthLoaded(store.getState())) {
       store.dispatch(loadAuth()).then(checkAuth);
     }
     else {
